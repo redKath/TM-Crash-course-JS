801dfd6561ea58cfca28727fadb4a3aab5eb49ab
Kathleen Jogno <jognokath@gmail.com>
2022-07-30 10:55:57 +0800

    Adden carousel exercise - for loop in main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // looping exercises
 // Carousel
 // Write a program that launches a carousel for 10 turns, showing the turn number each time.
-const turns;
-for (turns = 1; turns <= 10; turns++) {
-    console.log(turns)
+let turns;
+// for (turns = 1; turns <= 10; turns++) {
+//     console.log(turns)
+// }
 
+//When it's done, improve it so that the number of turns is given by the user.
+userTurns = prompt("How many turns?")
+for (turns=0; turns <= userTurns; turns++) {
+    console.log(turns)
 }
