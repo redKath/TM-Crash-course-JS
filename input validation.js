cc1c252c23f2e956990192852e2ac050c461019c
Kathleen Jogno <jognokath@gmail.com>
2022-07-31 11:20:47 +0800

    Start working on multiplication table exercise using loop

---
+++ input validation.js
@@ -0,0 +1,17 @@
+// Input validation
+
+// Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
+console.log("=========")
+// let num = prompt("Enter number");
+// while (num >100) {
+//   num = prompt("Enter number")
+//   console.log(num)
+// }
+// When you are done with the above, improve the program so that the terminating number is between 50 and 100.
+let num = prompt("Enter a number");
+while ((num < 50) || (num > 100)) {
+    console.log(num)
+    num = prompt("Enter number")
+
+}
+console.log("end")
--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-// Input validation
+// multiplication table
+//  Write a program that asks the user for a number, then shows the multiplication table for this number.
 
-// Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
-console.log("=========")
-// let num = prompt("Enter number");
-// while (num >100) {
-//   num = prompt("Enter number")
-//   console.log(num)
-// }
-// When you are done with the above, improve the program so that the terminating number is between 50 and 100.
-let num = prompt("Enter a number");
-while ((num <= 50) || (num >= 100)) {
-  num = prompt("Enter number")
-  console.log(num)
-}
-console.log("end")
+const number = prompt("Give a number: ");
+
