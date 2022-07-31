608a690af81db07053b14d8f38ad1e46275217e5
Kathleen Jogno <jognokath@gmail.com>
2022-07-31 11:58:11 +0800

    Added Fizzbuzz answer in main.js

--- main.js
+++ main.js
@@ -7,3 +7,20 @@
 //     It shows "Buzz" instead if the number is divisible by 5 and not by 3.
 
 // When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
+let number = prompt('Enter a number between 1-100');
+while ((number > 0) && (number < 101)) {
+    if ((number % 5 === 0) && (number % 3 === 0)) {
+        console.log(`FizzBuzz : ${number} `)
+        number = 0
+    } else if ((number % 5 === 0) && (number % 3 !== 0)) {
+        console.log(`Buzz : ${number} `)
+        number = 0
+    } else if ((number % 3 === 0)) {
+        console.log(`Fizz : ${number} `)
+        number = 0
+    } else {
+        console.log(`${number}`)
+        number = 0
+    }
+}
+console.log(`stopped : ${number}`)
