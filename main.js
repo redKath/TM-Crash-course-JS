8e4de8ea487e09258e0baf84b9e7b7c53db61162
Kathleen Jogno <jognokath@gmail.com>
2022-08-02 10:26:02 +0800

    Added answers in arrow functions task in main.js

--- main.js
+++ main.js
@@ -17,9 +17,53 @@ let currentUserName = 'John';
 
 const BIRTHDAY = '18.04.1982';
 
-const age = someCode(birthday);
+// const age = someCode(birthday);
 
-function someCode(birthday) {
-    // somecode
+// function someCode(birthday) {
+//     // somecode
+// }
+
+/* FUNCTIONS */
+// Write a function min(a, b) which returns the least of two numbers a and b.
+
+function min(a, b) {
+    if (a > b) {
+        return console.log(b)
+    } else if (b > a) {
+        return console.log(a)
+    } else {
+        return console.log(`${a} and ${b} are equal`)
+    }
 }
 
+min(2, 5)
+min(3, -1)
+min(1, 1)
+
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+function pow(x, n) {
+    return console.log(x ** n)
+}
+
+pow(3, 2) //= 3 * 3 = 9
+pow(3, 3) //= 3 * 3 * 3 = 27
+pow(1, 100) //= 1 * 1 * ...* 1 = 1
+
+//Replace Function Expressions with arrow functions in the code below:
+
+/*
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function () { alert("You agreed."); },
+    function () { alert("You canceled the execution."); }
+);*/
+
+let ask = (question, yes, no) => {  // the curly brace opens a multiline function
+    if (confirm(question)) yes();
+    else no();
+};
